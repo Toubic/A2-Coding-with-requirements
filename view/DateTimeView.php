2ad6d4cbb7d1f44a426b56beee543dcd3820707b
Toubic <tj22fe@student.lnu.se>
2016-09-26 05:14:09 +0200

    fix server time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date('r');
+		$timeString = date('l').", the ".date('jS \of F Y').", The time is ".date('h:i:s');
 
 		return '<p>' . $timeString . '</p>';
 	}
