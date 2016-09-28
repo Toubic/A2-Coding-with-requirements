014ed8326b9431f22b767bc70f7c96169ee3ed9d
Toubic <tj22fe@student.lnu.se>
2016-09-29 00:45:21 +0200

    Fix date time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,9 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date('l').", the ".date('jS \of F Y').", The time is ".date('h:i:s');
+        date_default_timezone_set('CET');
+
+		$timeString = date('l').", the ".date('jS \of F Y').", The time is ".date('H:i:s');
 
 		return '<p>' . $timeString . '</p>';
 	}
