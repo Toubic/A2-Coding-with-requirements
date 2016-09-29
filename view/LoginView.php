f0ac480fea5c712db34f85f5acaea47a775410a4
Toubic <tj22fe@student.lnu.se>
2016-09-29 02:14:29 +0200

    Fix error handling invalid username and password

--- view/LoginView.php
+++ view/LoginView.php
@@ -65,10 +65,10 @@ class LoginView {
         if(isset($_SESSION['isOnRegisterPage'])){
             if($this->rv->getRegisterSubmit()) {
                 if (is_string($registerUsername) && strlen($registerUsername) < 3) {
-                    $message = "Username has too few characters, at least 3 characters.<br>";
+                    $message = "Username has too few characters, at least 3 characters.";
                 }
                 if (is_string($registerPassword) && strlen($registerPassword) < 6) {
-                    $message .= "Password has too few characters, at least 6 characters.<br>";
+                    $message .= "Password has too few characters, at least 6 characters.";
                 }
                 $response = $this->rv->generateRegisterNewUserHTML($message);
                 return $response;
