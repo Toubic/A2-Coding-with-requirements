76b24923b8b8d4ff1c985ce7210b7333010d7531
Toubic <tj22fe@student.lnu.se>
2016-10-20 00:58:49 +0200

    fix gitignore

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array(
-    "databaseDetails" => "host=ec2-54-75-228-51.eu-west-1.compute.amazonaws.com port=5432 dbname=dfamvr9489el11 user=bkmuesonvzihku password= SQ9eCnS1Y0UqO9t0qZ0clDO4nn sslmode=require"
-);
